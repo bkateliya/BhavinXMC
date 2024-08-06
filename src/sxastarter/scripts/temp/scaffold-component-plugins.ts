0eabfe2fbda9bc1d24da07e5804420e6ca4083ee
bkateliya <bkateliya@horizontal.com>
2024-08-06 13:22:37 +0530

    code push

---
+++ src/sxastarter/scripts/temp/scaffold-component-plugins.ts
@@ -0,0 +1,4 @@
+export { byocPlugin } from 'scripts/scaffold-component/plugins/byoc';
+export { componentPlugin } from 'scripts/scaffold-component/plugins/component';
+export { nextStepsByocPlugin } from 'scripts/scaffold-component/plugins/next-steps-byoc';
+export { nextStepsPlugin } from 'scripts/scaffold-component/plugins/next-steps';
